62ddc51e5d3cbd46c750a28265dd577de96ffd6b
valeriofilareti <valerio.filareti@gmail.com>
2023-05-22 13:58:08 +0200

    [add] create movie class

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Movie {
+  public $title;
+  public $director;
+  public $genre;
+  function __construct($_title, $_director){
+    $this->name = $_name;
+    $this->director = $_director;
+  }
+}
+
